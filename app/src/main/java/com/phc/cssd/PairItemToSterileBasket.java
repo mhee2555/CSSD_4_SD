b7d91cf2fda58b9c09dcd513535b5fc299aa8486
toy <saran.toy@hotmail.com>
2020-07-20 10:22:14 +0700

    ก่อนเข้าเครื่องฆ่าเชื้ออุปกรณ์ทุกชิ้นต้องอยู่ใน basket tag --1

--- app/src/main/java/com/phc/cssd/PairItemToSterileBasket.java
+++ app/src/main/java/com/phc/cssd/PairItemToSterileBasket.java
@@ -1,9 +1,10 @@
 package com.phc.cssd;
 
+import android.app.Activity;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class PairItemToSterileBasket extends AppCompatActivity {
+public class PairItemToSterileBasket extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
