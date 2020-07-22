55366a9f60978a1a96485997bd0cac1db786a0a7
poramut <38275061+poramut@users.noreply.github.com>
2020-07-22 11:39:30 +0700

    อัพเดตส่งล้าง

---
+++ app/src/main/java/com/phc/cssd/dialog_linen_detail.java
@@ -0,0 +1,15 @@
+package com.phc.cssd;
+
+
+import android.app.Activity;
+import android.os.Bundle;
+import com.phc.cssd.R;
+
+public class dialog_linen_detail extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_dialog_linen_detail);
+    }
+}
