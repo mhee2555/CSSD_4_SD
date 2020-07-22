7ecc6a26989753a2282518e9d45ac977328e922b
poramut <38275061+poramut@users.noreply.github.com>
2020-07-22 13:57:52 +0700

    อัพเดตส่งล้าง

--- app/src/main/java/com/phc/cssd/dialog_linen_detail.java
+++ app/src/main/java/com/phc/cssd/dialog_linen_detail.java
@@ -3,13 +3,35 @@ package com.phc.cssd;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.EditText;
+import android.widget.ListView;
+
 import com.phc.cssd.R;
 
 public class dialog_linen_detail extends Activity {
+
+    Button back,save;
+    EditText search_name,search_scan;
+    CheckBox chk_all;
+    ListView item;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dialog_linen_detail);
+
+        initialize();
+
+    }
+
+    private void initialize(){
+        back = (Button) findViewById(R.id.back);
+        save = (Button) findViewById(R.id.save);
+        search_name = (EditText) findViewById(R.id.search_name);
+        search_scan = (EditText) findViewById(R.id.search_scan);
+        chk_all = (CheckBox) findViewById(R.id.chk_all);
+        item = (ListView) findViewById(R.id.item);
     }
 }
