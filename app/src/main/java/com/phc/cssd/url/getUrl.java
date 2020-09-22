55f284a5d719ff69539830a420ba0a1f8515843b
poramut <38275061+poramut@users.noreply.github.com>
2020-09-22 12:53:03 +0700

    อัพเดค แจ้งเตือน

--- app/src/main/java/com/phc/cssd/url/getUrl.java
+++ app/src/main/java/com/phc/cssd/url/getUrl.java
@@ -12,7 +12,7 @@ public class getUrl {
     // ----------------------------------------------------
     public static final int appversion = 12;
 
-    public static final String VERSION = "4U" + "20200902 B";
+    public static final String VERSION = "4U" + "20200922 B";
     // ----------------------------------------------------
     // Var URL
     // ----------------------------------------------------
